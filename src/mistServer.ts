0f26d9685f4eb54f69e068e1b5f19a9a99669946
Zhenjie Yan <im.yanzhenjie@gmail.com>
2021-05-11 17:57:33 +0800

    统一优化Android和iOS的mist调试体验,升级图标适配vscode1.56.

--- src/mistServer.ts
+++ src/mistServer.ts
@@ -1,26 +1,18 @@
 import * as fs from 'fs'
 import * as http from 'http'
 import { compile } from 'mistc'
+import * as os from 'os'
 import * as path from 'path'
+import * as QRCode from 'qrcode'
 import * as request from 'request'
 import * as vscode from 'vscode'
 import { commands, ExtensionContext } from 'vscode'
 import { isMistFile } from './previewProvider'
-import * as QRCode from 'qrcode'
-import * as os from 'os'
-import * as bonjour from 'bonjour'
 
 export let stopServerFunc: () => void
 
-let iOSConfig = {
-  debugingiOS: false,
-  clientAddress: undefined,
-  clientPort: 10002
-}
-
 enum CommandContext {
-  IsDebugging = 'mist:isDebugging',
-  DebugingiOS = 'mist:debugingiOS'
+  IsDebugging = 'mist:isDebugging'
 }
 
 function setCommandContext(key: CommandContext | string, value: any) {
@@ -51,14 +43,14 @@ function registerServer(context: ExtensionContext) {
       let serverPort = 10001
       server = http.createServer(async (req, res) => {
         output.appendLine(`> ${req.method}\t${req.url}`)
-        if (req.url === "/") {
+        if (req.url === '/') {
           res.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' })
-          res.end("It works!")
+          res.end('It works!')
           return
         }
 
         clientAddress = req.connection.remoteAddress
-        
+
         const file = path.join(workingDir, req.url)
         try {
           if (!fs.existsSync(file)) {
@@ -68,16 +60,18 @@ function registerServer(context: ExtensionContext) {
           let content: string
           if (path.extname(file) === '.mist') {
             try {
-              content = await compile(file, { platform: 'ios', debug: true }) as string
+              content = (await compile(file, { platform: 'ios', debug: true })) as string
             } catch (e) {
               output.appendLine(e)
               const name = req.url.replace(/^\//, '')
-              vscode.window.showErrorMessage(`'${name}' 模板编译失败，请修复错误后重新请求。${e}`, '打开该模板').then(async r => {
-                const doc = await vscode.workspace.openTextDocument(file)
-                if (doc) {
-                  vscode.window.showTextDocument(doc)
-                }
-              })
+              vscode.window
+                .showErrorMessage(`'${name}' 模板编译失败，请修复错误后重新请求。${e}`, '打开该模板')
+                .then(async (r) => {
+                  const doc = await vscode.workspace.openTextDocument(file)
+                  if (doc) {
+                    vscode.window.showTextDocument(doc)
+                  }
+                })
             }
           } else if (path.extname(file) === '.png') {
             fs.readFile(file, 'binary', function (err, file) {
@@ -120,7 +114,7 @@ function registerServer(context: ExtensionContext) {
         let localAddress = undefined
         if (nets.hasOwnProperty('en0')) {
           for (let index = 0; index < nets.en0.length; index++) {
-            const net = nets.en0[index];
+            const net = nets.en0[index]
             if (net.family === 'IPv4' && !net.internal) {
               localAddress = net.address
               break
@@ -130,7 +124,9 @@ function registerServer(context: ExtensionContext) {
 
         output = vscode.window.createOutputChannel('Mist Debug Server')
         output.show()
-        output.appendLine(`> Start mist debug server at http://${localAddress? localAddress : "127.0.0.1"}:${serverPort}`)
+        output.appendLine(
+          `> Start mist debug server at http://${localAddress ? localAddress : '127.0.0.1'}:${serverPort}`
+        )
         if (localAddress) {
           const qrcodePath = '/tmp/mist.png'
           let config = {
@@ -141,9 +137,8 @@ function registerServer(context: ExtensionContext) {
           output.appendLine('> 如果使用设备调试，记得配置调试参数，或者扫码配置。')
           output.appendLine(`> QRCode: file://${qrcodePath}  ← Please hold command and click`)
         } else {
-          output.appendLine('Can\'t find local address')
+          output.appendLine("Can't find local address")
         }
-        
       })
     })
   )
@@ -156,21 +151,9 @@ function registerServer(context: ExtensionContext) {
 
   context.subscriptions.push(
     vscode.workspace.onDidSaveTextDocument((document) => {
-
-      // if (isMistFile(document)) {
-      //   // Mist文件保存后自动触发推送到Android设备，免去手动点击Android机器人图标
-      //   pushTemplateToAndroid()
-      //   console.log("push template to Android: " + document.uri.path);
-      // } 
-
       let validFormat = isMistFile(document) || document.uri.path.endsWith('.json')
       if (!validFormat) {
         return
-      }
-
-      
-      if (iOSConfig.debugingiOS) {
-        pushTemplateToiOS(document.fileName)
       }
 
       if (!server) {
@@ -188,7 +171,6 @@ function registerServer(context: ExtensionContext) {
         console.log(`SIMULATOR NOT RESPONSE: ${e.message}\n`)
       })
       req.end()
-
     })
   )
 
@@ -208,6 +190,24 @@ function registerServer(context: ExtensionContext) {
     }
   }
   stopServerFunc = stopServer
+}
+
+function registerPushService(context: ExtensionContext) {
+  context.subscriptions.push(
+    commands.registerCommand('mist.debugAndroid', (args) => {
+      console.info('click debug.Android')
+      require('child_process').exec("adb shell ip route | awk '{printf $9}'", async function (error, stdout, stderr) {
+        pushTemplateToPhone(stdout, 'android')
+      })
+    })
+  )
+
+  context.subscriptions.push(
+    commands.registerCommand('mist.debugIOS', (args) => {
+      console.info('click debug.iOS')
+      pushTemplateToPhone(null, 'ios')
+    })
+  )
 }
 
 /**
@@ -263,160 +263,22 @@ async function insertInEditor(path: string, name: string) {
   editor.insertSnippet(new vscode.SnippetString(value), new vscode.Position(0, 1))
 }
 
-function registerPushService(context: ExtensionContext) {
-  context.subscriptions.push(
-    commands.registerCommand('mist.debugAndroid', (args) => {
-      pushTemplateToAndroid()
-    })
-  )
+/**
+ * 推送模板到目标端
+ * @param deviceIp 目标设备ip地址
+ * @param platform 目标设备端口
+ */
+async function pushTemplateToPhone(deviceIp, platform) {
+  let fileUri = vscode.window.activeTextEditor.document.uri
+  let mistFile = decodeURI(fileUri.toString().substring(7))
+  let mistPath = path.parse(mistFile)
+  let configFile = mistPath.dir + '/config.json'
 
-  let browser
-  context.subscriptions.push(
-    commands.registerCommand('mist.startiOS', (args) => {
-      setCommandContext(CommandContext.DebugingiOS, true)
-      iOSConfig.debugingiOS = true
-      if (browser) {
-        browser.stop()
-      }
-      browser = bonjour().find({ type: 'mistdev', protocol: 'tcp' }, function (service) {
-        let address = service.referer.address
-        vscode.window.showInformationMessage(`检查到可调式设备 ${address} (${service.name})，是否连接？`, "连接", "忽略")
-        .then((selected) => {
-          if ('连接' !== selected) {
-            return
-          }
-          iOSConfig.clientAddress = address
-          uppdateClientAddress(address)
-          vscode.window.showInformationMessage(`成功连接到设备 ${address}`)
-        })
-      })
-    })
-  )
-
-  context.subscriptions.push(
-    commands.registerCommand('mist.stopiOS', (args) => {
-      setCommandContext(CommandContext.DebugingiOS, false)
-      iOSConfig.debugingiOS = false
-      iOSConfig.clientAddress = undefined
-      if (browser) {
-        browser.stop()
-        browser = null
-      }
-    })
-  )
-}
-
-function pushTemplateToAndroid(){
-     // push current file to Android
-     require('child_process').exec("adb shell ip route | awk '{printf $9}'", async function (error, stdout, stderr) {
-      let deviceIp = stdout
-      // let ptr = stdout.indexOf('scope link src ')
-      // if (ptr > 0) {
-      //   ptr = ptr + 'scope link src '.length
-      //   deviceIp = stdout.substr(ptr).trim()
-      // }
-
-      let fileUri = vscode.window.activeTextEditor.document.uri
-      let mistFile = decodeURI(fileUri.toString().substring(7))
-      let mistPath = path.parse(mistFile)
-      let configFile = mistPath.dir + '/config.json'
-
-      if (!fs.existsSync(configFile)) {
-        vscode.window.showErrorMessage('请配置业务前缀bizCode并保存。')
-        insertInEditor(configFile, 'bizCode')
-        return
-      }
-
-      let config
-      try {
-        config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
-      } catch (e) {
-        openEditor(configFile)
-        vscode.window.showErrorMessage('请确保config.json文件内容格式为JSON。')
-        return
-      }
-
-      const noBizCode = config.noBizCode
-      const bizCode = config.bizCode
-      if (!noBizCode && !bizCode) {
-        vscode.window.showErrorMessage('请配置业务前缀bizCode并保存，无业务前缀请配置noBizCode: true。')
-        insertInEditor(configFile, 'bizCode')
-        return
-      }
-
-      if (!deviceIp) {
-        deviceIp = config.deviceIp
-      }
-      if (!deviceIp) {
-        vscode.window.showErrorMessage('请配置设备IP地址或者连接ADB。')
-        insertInEditor(configFile, 'deviceIp')
-        return
-      }
-
-      let templateContent
-      try {
-        templateContent = await compile(mistFile, { minify: true, platform: 'android', debug: true })
-      } catch (e) {
-        vscode.window.showErrorMessage('模板编译错误：' + e.message)
-        return
-      }
-
-      const imagesDir = mistPath.dir + '/Images'
-      const images = await readFiles(imagesDir)
-      const filePaths = generatePathArray(imagesDir, images)
-      const dataPath = generateDataPath(mistPath.dir, config.dataPath)
-      filePaths.push({
-        name: 'mockData.json',
-        path: dataPath
-      })
-
-      const templateName = noBizCode ? mistPath.base : config.bizCode + '@' + mistPath.name
-      const formData = generateFromData(filePaths, templateName, templateContent)
-
-      const devicePort = config.devicePort
-      const deviceUrl = `http://${deviceIp}:${devicePort ? parseInt(devicePort) : 9012}/update`
-      postForm(deviceUrl, formData, (err, res, data) => {
-        if (err) {
-          vscode.window.showErrorMessage('请求手机失败：' + err)
-        } else if (data) {
-          data = JSON.parse(data)
-          if (data.success == true) {
-            vscode.window.showInformationMessage('模板已传输到手机.')
-          } else if (data.message) {
-            vscode.window.showErrorMessage('传输模板到手机失败：' + data.message)
-          }
-        } else {
-          vscode.window.showErrorMessage('传输模板到手机失败: 未知错误!')
-        }
-      })
-    })
-}
-
-function showErrorAndEdit(message) {
-  vscode.window.showErrorMessage(`${message}，是否手动配置？`, "配置", "忽略")
-    .then((selected) => {
-      if ('配置' !== selected) {
-        return
-      }
-      let configFile = `${os.homedir()}/mist_config.json`
-      insertInEditor(configFile, 'deviceIp')
-    })
-}
-
-function readClientAddress() {
-  let configFile = `${os.homedir()}/mist_config.json`
-  let config
-  try {
-    config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
-  } catch (e) {
-    vscode.window.showErrorMessage('请确保config.json文件内容格式为JSON。')
-    return undefined
+  if (!fs.existsSync(configFile)) {
+    fs.closeSync(fs.openSync(configFile, `w`))
+    fs.writeFileSync(configFile, '{}')
   }
-  return config.deviceIp
-}
 
-function uppdateClientAddress(address) {
-  let configFile = `${os.homedir()}/mist_config.json`
   let config
   try {
     config = JSON.parse(fs.readFileSync(configFile, 'utf-8'))
@@ -425,63 +287,99 @@ function uppdateClientAddress(address) {
     vscode.window.showErrorMessage('请确保config.json文件内容格式为JSON。')
     return
   }
-  config.deviceIp = address
-  fs.writeFileSync(configFile, JSON.stringify(config), 'utf-8')
-}
 
-function pushTemplateToiOS(filePath) {
-  if (!iOSConfig.clientAddress) {
-    iOSConfig.clientAddress = readClientAddress()
+  if (!deviceIp) {
+    deviceIp = config.deviceIp
   }
-  if (!iOSConfig.clientAddress) {
-    // 手动选择设备
-    showErrorAndEdit('没有已连接的手机')
+  if (!deviceIp) {
+    vscode.window.showErrorMessage('未检测到手机IP，请配置手机IP地址。')
+    insertInEditor(configFile, 'deviceIp')
     return
   }
-  let templateName = filePath.substr(filePath.lastIndexOf('/')+1)
-  vscode.window.showInformationMessage(`${templateName} => ${iOSConfig.clientAddress}`)
 
-  compile(filePath, { platform: 'ios', debug: true })
-  .then(function(templateContent) {
-    const formData = generateFromData([], templateName, templateContent)
-    const deviceUrl = `http://${iOSConfig.clientAddress}:${iOSConfig.clientPort}/update`
-    postForm(deviceUrl, formData, (err, res, data) => {
-      if (err) {
-        showErrorAndEdit(`请求手机失败 ${iOSConfig.clientAddress}：${err}`)
-      } else if (data) {
-        const parsedData = JSON.parse(data)
-        if (parsedData.success == true) {
-          vscode.window.showInformationMessage('模板已传输到手机.')
-        } else if (parsedData.message) {
-          vscode.window.showErrorMessage(`传输模板到手机失败：${parsedData.message}`)
-        } else {
-          vscode.window.showErrorMessage(`传输出错：${data}`)
-        }
-      } else {
-        vscode.window.showErrorMessage('传输模板到手机失败: 未知错误!')
-      }
-    })
+  const noBizCode = config.noBizCode
+  const bizCode = config.bizCode
+  if (!noBizCode && !bizCode) {
+    vscode.window.showErrorMessage('请配置业务前缀bizCode，无业务前缀请配置noBizCode: true。')
+    insertInEditor(configFile, 'bizCode')
+    return
+  }
+
+  let templateContent
+  try {
+    templateContent = await compile(mistFile, { minify: true, platform: platform, debug: true })
+  } catch (e) {
+    vscode.window.showErrorMessage('模板编译不通过：' + e.message)
+    return
+  }
+
+  const imagesDir = mistPath.dir + '/Images'
+  const images = await readFiles(imagesDir)
+  const filePaths = generatePathArray(imagesDir, images)
+  const dataPath = generateDataPath(mistPath.dir, config.dataPath)
+  filePaths.push({
+    name: 'mockData.json',
+    path: dataPath
   })
-  .catch(function(e) {
-    vscode.window.showErrorMessage(`'${filePath}' 模板编译失败，请修复错误后重新请求。${e}`, '打开该模板').then(async r => {
-      const doc = await vscode.workspace.openTextDocument(filePath)
-      if (doc) {
-        vscode.window.showTextDocument(doc)
-      }
-    })
-  })
+
+  const templateName = noBizCode ? mistPath.base : config.bizCode + '@' + mistPath.name
+  const formData = generateFromData(filePaths, templateName, templateContent)
+  const devicePort = config.devicePort ? parseInt(config.devicePort) : 9012
+  const deviceUrl = `http://${deviceIp}:${devicePort}/update`
+  vscode.window.withProgress(
+    {
+      location: vscode.ProgressLocation.Notification,
+      cancellable: false,
+      title: 'MIST预览'
+    },
+    (progress, token) => {
+      progress.report({ message: '正在连接手机' })
+      const promise = new Promise<void>((resolve) => {
+        postForm(deviceUrl, formData, 3000, (err, res, data) => {
+          if (err) {
+            if (err.code == 'ETIMEDOUT') {
+              vscode.window.showErrorMessage(`连接手机超时，请确保IP与端口无误：${deviceIp}:${devicePort}`)
+            } else if (err.code == 'ECONNREFUSED') {
+              vscode.window.showErrorMessage(`手机拒绝连接，Server挂了，请重启App：${deviceIp}`)
+            } else {
+              vscode.window.showErrorMessage(`连接手机失败，未知异常：${err}`)
+            }
+          } else if (data) {
+            data = JSON.parse(data)
+            if (data.success == true) {
+              progress.report({ increment: 100, message: '模板已传输到手机' })
+            } else if (data.message) {
+              vscode.window.showErrorMessage(`模板传输失败：${data.message}`)
+            }
+          } else {
+            vscode.window.showErrorMessage('模板传输失败: 未知错误!')
+          }
+          setTimeout(
+            () => {
+              resolve()
+            },
+            data && data.success ? 1500 : 0
+          )
+        })
+      })
+      token.onCancellationRequested(() => {
+        console.log('User canceled the preview mist operation')
+      })
+      return promise
+    }
+  )
 }
 
 /**
  * 生成数据文件路径
  * @param dir 父目录路径string
- * @param configPath config.json配置的dataPath
+ * @param dataPath config.json配置的dataPath
  */
-function generateDataPath(dir, configPath) {
-  if (configPath) {
-    configPath = path.join(dir, configPath)
+function generateDataPath(dir, dataPath) {
+  if (dataPath && dataPath.indexOf('/') != 0) {
+    dataPath = path.join(dir, dataPath)
   }
-  return configPath ? configPath : dir + '/mockData.json'
+  return dataPath ? dataPath : path.join(dir, './mockData.json')
 }
 
 /**
@@ -515,9 +413,10 @@ function generateFromData(filePaths, templateName, templateContent) {
  * @param formData 表单对象
  * @param callback 回调函数
  */
-function postForm(url, formData, callback) {
+function postForm(url, formData, timeout, callback) {
   request.post(
     {
+      timeout: timeout,
       url: url,
       headers: { 'Content-Type': 'multipart/form-data' },
       formData: formData
